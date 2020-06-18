3ab357bc45ba216fb8eb14a07c7e38a8775d5be4
Windy <laureneuge@Laurens-MBP-17.home>
2020-06-17 21:26:49 -0700

    drag and drop and touch punch

--- assets/js/script.js
+++ assets/js/script.js
@@ -253,3 +253,18 @@ saveTasks();
 console.log(tempArr);
     }
   });
+
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      ui.draggable.remove();
+      console.log("drop");
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
